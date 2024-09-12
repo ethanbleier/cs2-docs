49241e1316dfc8e49006ad8cf42b9858282f5d09
ethan bleier <ebleier4@gmail.com>
2024-09-11 20:55:38 -0700

    working

--- cs2-docs/cs2-docs/src/components/Footer.js
+++ cs2-docs/cs2-docs/src/components/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer>
+		<footer style={{ fontSize: '0.33rem', padding: '5px', textAlign: 'center' }}>
+			<p>🏴‍☠️</p>
 			<p>&copy; 2024 CS2 Console Docs. All rights reserved.</p>
 		</footer>
 	);
